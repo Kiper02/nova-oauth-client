fb40348927a7ddd23231bb3d3528c6c8c6e17ec8
kiper <permakovkirill925@gmail.com>
2025-02-28 22:58:03 +0500

    Получение scope с сервера ouath2

--- src/shared/components/applications/CreateApplicationStepTwo.tsx
+++ src/shared/components/applications/CreateApplicationStepTwo.tsx
@@ -1,22 +1,42 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import ProcessLine from "./ProcessLine";
+import applicationsService from "@/shared/services/applications.service";
 
 interface ICreateApplicationStepTwoProps {
   setScopes: Dispatch<SetStateAction<string[]>>;
-  scopes: string[]
+  scopes: string[];
   setPage: Dispatch<SetStateAction<number>>;
 }
 
-export default function CreateApplicationStepTwo({setScopes,scopes, setPage}: ICreateApplicationStepTwoProps) {
+interface IScope {
+  id: string;
+  name: string;
+  description: string;
+}
+
+export default function CreateApplicationStepTwo({
+  setScopes,
+  scopes,
+  setPage,
+}: ICreateApplicationStepTwoProps) {
+  const [valueScope, setValueScope] = useState<IScope[]>([]);
 
   const addScope = (scope: string) => {
     setScopes([...scopes, scope]);
-  }
+  };
 
+  const findScope = async () => {
+    const response = await applicationsService.findScope();
+    setValueScope(response);
+  };
+
+  useEffect(() => {
+    findScope();
+  }, []);
 
   return (
     <>
-    <ProcessLine percent={50} />
+      <ProcessLine percent={50} />
       <h1
         className="
             text-3xl 
@@ -32,27 +52,34 @@ export default function CreateApplicationStepTwo({setScopes,scopes, setPage}: IC
       >
         Выберите данные, доступ к которым вам нужен
       </h3>
-      <div className="
+      <div
+        className="
                 flex
                 flex-col
-                gap-2">
-        <div className="flex items-center">
-          <input
-            onChange={() => addScope("date:birth")}
-            id="default-checkbox"
-            type="checkbox"
-            value=""
-            className="
-            w-5 h-5 appearance-none rounded-sm bg-[#D9D9D9] border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 checked:bg-[#6200FF]"
-          />
-          <label
-            htmlFor="default-checkbox"
-            className="ms-2 text-[16px] font-medium text-black"
-          >
-            Доступ к дате рождения
-          </label>
-        </div>
-        <div className="flex items-center">
+                gap-2"
+      >
+        {
+          valueScope &&
+          valueScope.map(item => 
+            <div className="flex items-center" key={item.id}>
+            <input
+              onChange={() => addScope(item.name)}
+              id="default-checkbox"
+              type="checkbox"
+              value=""
+              className="
+              w-5 h-5 appearance-none rounded-sm bg-[#D9D9D9] border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 checked:bg-[#6200FF]"
+            />
+            <label
+              htmlFor="default-checkbox"
+              className="ms-2 text-[16px] font-medium text-black"
+            >
+              {item.description}
+            </label>
+          </div>
+          )
+        }
+        {/* <div className="flex items-center">
           <input
             onChange={() => addScope("email")}
             id="default-checkbox"
@@ -86,7 +113,7 @@ export default function CreateApplicationStepTwo({setScopes,scopes, setPage}: IC
         </div>
         <div className="flex items-center">
           <input
-          onChange={() => addScope("avatar")}
+            onChange={() => addScope("avatar")}
             id="default-checkbox"
             type="checkbox"
             value=""
@@ -102,7 +129,7 @@ export default function CreateApplicationStepTwo({setScopes,scopes, setPage}: IC
         </div>
         <div className="flex items-center">
           <input
-          onChange={() => addScope("phone")}
+            onChange={() => addScope("phone")}
             id="default-checkbox"
             type="checkbox"
             value=""
@@ -115,7 +142,7 @@ export default function CreateApplicationStepTwo({setScopes,scopes, setPage}: IC
           >
             Доступ к номеру телефона
           </label>
-        </div>
+        </div> */}
       </div>
       <button
         onClick={() => setPage(3)}
--- src/shared/services/applications.service.ts
+++ src/shared/services/applications.service.ts
@@ -2,24 +2,31 @@ import type { LoginType, RegisterType } from "@/shared/types/auth.type";
 import axios from "axios";
 
 class ApplicationsService {
-  private authUrl = `${process.env.NEXT_PUBLIC_API_URL}/session`;
+  private authUrl = `${process.env.NEXT_PUBLIC_API_URL}`;
 
   public async create(dto: RegisterType) {
-    const response = await axios.post(`${this.authUrl}/register`, dto, {
+    const response = await axios.post(`${this.authUrl}/session/register`, dto, {
       withCredentials: true,
     });
     return response.data;
   }
 
   public async findAll(dto: LoginType) {
-    const response = await axios.post(`${this.authUrl}/login`, dto, {
+    const response = await axios.post(`${this.authUrl}/session/login`, dto, {
       withCredentials: true,
     });
     return response.data;
   }
 
   public async delete(dto: LoginType) {
-    const response = await axios.post(`${this.authUrl}/login`, dto, {
+    const response = await axios.post(`${this.authUrl}/session/login`, dto, {
+      withCredentials: true,
+    });
+    return response.data;
+  }
+
+  public async findScope() {
+    const response = await axios.get(`${this.authUrl}/application/scope`, {
       withCredentials: true,
     });
     return response.data;
